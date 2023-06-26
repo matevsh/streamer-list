30207bd25f42c8b7a0c9cf50b7122f22e921c880
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-26 16:15:26 +0200

    feat: user check route for identify user

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async checkSession(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const cookieUserId = request.cookies['userId'];
+
+    if (cookieUserId) {
+      const dbRes = await this.userService.checkUserId(cookieUserId);
+      if (dbRes) return { success: true };
+    }
+
+    const generatedUser = await this.userService.createUser();
+    if (generatedUser) {
+      response.cookie('userId', generatedUser.uuid);
+      return { success: true };
+    }
+
+    return { success: false };
+  }
+}
--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+
+import { PrismaService } from '../common/prisma/prisma.service';
+
+type UserOrErr = User | null;
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  createUser(): Promise<UserOrErr> {
+    return this.prisma.user.create({ data: {} }).catch(() => null);
+  }
+
+  checkUserId(userId: string): Promise<UserOrErr> {
+    return this.prisma.user
+      .findFirst({
+        where: {
+          uuid: userId,
+        },
+      })
+      .catch(() => null);
+  }
+}
