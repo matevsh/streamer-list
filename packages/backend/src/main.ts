a8035bd377128372c411741ae8ccc133d97fc566
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-26 17:25:31 +0200

    feat: global validation pipe

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
