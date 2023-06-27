1717e3f9694939530e24144a14178fae9d8cb061
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-27 22:04:55 +0200

    feat: fetcher with schema validation

---
+++ packages/frontend/src/common/utils/fetcher.ts
@@ -0,0 +1,15 @@
+import {z} from "zod";
+
+export async function fetcher<T>(url: string, schema: z.ZodType<T>) {
+    const response = await fetch(url, {
+        headers: { Accept: 'application/json+queries' },
+        method: "GET"
+    })
+
+    if(!response.ok) {
+        throw new Error(`Response with status ${response.status} is not ok.`)
+    }
+
+    const data = await response.json();
+    return schema.parse(data)
+}
