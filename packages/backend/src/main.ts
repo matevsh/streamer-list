c680e96acbc3447c6b298b164dab7bae4fe3a702
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-29 12:09:04 +0200

    feat: add cors with credentials

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
