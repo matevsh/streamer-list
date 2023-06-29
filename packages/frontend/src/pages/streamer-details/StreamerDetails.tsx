fff19ec3d867129704e023acd03b9919f67560b8
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-29 14:50:18 +0200

    feat: add navigation in streamer details

--- packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
+++ packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
@@ -1,9 +1,21 @@
+import {useParams, Navigate, Link} from "react-router-dom";
+
 import styles from './streamer-details.module.css'
+import {IconArrowNarrowLeft} from "@tabler/icons-react";
 
 export function StreamerDetails() {
+    const {id: streamerId} = useParams()
+    if(!streamerId) return <Navigate to={'/'} />
+
     return (
-        <div className={styles.container}>
-            <h1>Streamer Details</h1>
-        </div>
+        <section className={styles.container}>
+            <Link to={'/'} className={styles.navigation}>
+                <IconArrowNarrowLeft />
+                <span>Powróć do listy</span>
+            </Link>
+            <div className={styles.content}>
+                <h1>Streamer Details</h1>
+            </div>
+        </section>
     )
 }
