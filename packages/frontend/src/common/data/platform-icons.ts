f1298513f49d4b96d417ef4889f23d43995f6412
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-29 19:23:52 +0200

    feat: add styles, view for streamer details

---
+++ packages/frontend/src/common/data/platform-icons.ts
@@ -0,0 +1,41 @@
+import {
+    IconApiApp,
+    IconBrandKick,
+    IconBrandRumble,
+    IconBrandTiktok,
+    IconBrandTwitch,
+    IconBrandYoutube
+} from "@tabler/icons-react";
+
+const platform = {
+    twitch: {
+        icon: IconBrandTwitch,
+        color: "#6441a5",
+    },
+    youtube: {
+        icon: IconBrandYoutube,
+        color: "#FF0000",
+    },
+    kick: {
+        icon: IconBrandKick,
+        color: "#52fb18",
+    },
+    tiktok: {
+        icon: IconBrandTiktok,
+        color: "#FFFFFF",
+    },
+    rumble: {
+        icon: IconBrandRumble,
+        color: "#85c742"
+    },
+    default: {
+        icon: IconApiApp,
+        color: "#FFFFFF"
+    }
+}
+
+export function getPlatformIcon(key: string | undefined) {
+    const icon = platform[key as keyof typeof platform];
+    const value = icon || platform.default
+    return [value.icon, value.color] as const
+}
--- packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
+++ packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
@@ -1,15 +1,18 @@
 import {useParams, Link} from "react-router-dom";
+import {IconArrowNarrowLeft} from "@tabler/icons-react";
 
 import styles from './streamer-details.module.css'
-import {IconArrowNarrowLeft} from "@tabler/icons-react";
 import {useStreamerDetails} from "./queries/use-streamer-details.ts";
+import {DEFAULT_IMAGE} from "../../common/constants/default-image.ts";
+import {getPlatformIcon} from "../../common/data/platform-icons.ts";
 
 export function StreamerDetails() {
     const {id: streamerId} = useParams()
-
     const {isLoading, data} = useStreamerDetails(streamerId)
 
     if(isLoading) return <>ładowanie...</>
+
+    const [Icon, color] = getPlatformIcon(data?.data.platform)
 
     return (
         <section className={styles.container}>
@@ -18,7 +21,15 @@ export function StreamerDetails() {
                 <span>Powróć do listy</span>
             </Link>
             <div className={styles.content}>
-                {data?.data.name}
+                <div className={styles.imageContainer}>
+                    <div className={styles.icon}><Icon /></div>
+                    <img src={DEFAULT_IMAGE} alt="streamer image"/>
+                </div>
+                <div>
+                    <p style={{color}} className={styles.platform}>{data?.data.platform}</p>
+                    <p className={styles.title}>{data?.data.name}</p>
+                    <span>{data?.data.description}</span>
+                </div>
             </div>
         </section>
     )
