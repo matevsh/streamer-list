bc0c5145c98a68aae620d5af1ed259f84edf2dd7
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-29 20:22:39 +0200

    feat: add votes display to streamer details page

--- packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
+++ packages/frontend/src/pages/streamer-details/StreamerDetails.tsx
@@ -1,5 +1,5 @@
 import {useParams, Link} from "react-router-dom";
-import {IconArrowNarrowLeft} from "@tabler/icons-react";
+import {IconArrowBigDownFilled, IconArrowBigUpFilled, IconArrowNarrowLeft} from "@tabler/icons-react";
 
 import styles from './streamer-details.module.css'
 import {useStreamerDetails} from "./queries/use-streamer-details.ts";
@@ -21,17 +21,28 @@ export function StreamerDetails() {
                 <span>Powróć do listy</span>
             </Link>
             <div className={styles.content}>
-                <div className={styles.headerContent}>
                     <div className={styles.imageContainer}>
                         <div className={styles.icon}><Icon /></div>
                         <img src={DEFAULT_IMAGE} alt="streamer image"/>
                     </div>
-                    <div>
-                        <p style={{color}} className={styles.platform}>{data?.data.platform}</p>
-                        <p className={styles.title}>{data?.data.name}</p>
-                        <span>{data?.data.description}</span>
+                    <div className={styles.textContent}>
+                        <div>
+                            <p style={{color}} className={styles.platform}>{data?.data.platform}</p>
+                            <p className={styles.title}>{data?.data.name}</p>
+                            <span>{data?.data.description}</span>
+                        </div>
+
+                        <div className={styles.votes}>
+                            <div>
+                                <IconArrowBigUpFilled style={{color: data?.data.vote.up.voted ? "green": "unset"}} />
+                                <span>{data?.data.vote.up.amount}</span>
+                            </div>
+                            <div>
+                                <IconArrowBigDownFilled style={{color: data?.data.vote.down.voted ? "red": "unset"}}/>
+                                <span>{data?.data.vote.down.amount}</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
         </section>
     )
