2b73703cc70233dc3850f9fd5227eae56797206e
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-30 21:09:08 +0200

    feat: cors origin read from env

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { StreamerModule } from './streamer/streamer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule, UserModule, StreamerModule],
+  imports: [CommonModule, UserModule, StreamerModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
