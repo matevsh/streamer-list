8f0fe14a1f0dd126d5862a684cbab9fc52356699
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-30 19:44:00 +0200

    refactor: remove textarea optionfrom input

--- packages/frontend/src/common/components/input/Input.tsx
+++ packages/frontend/src/common/components/input/Input.tsx
@@ -1,23 +1,20 @@
 import styles from './input.module.css'
+import {forwardRef} from "react";
 
 interface Props {
-    textarea: boolean
     id: string
     label?: string
     error: string | undefined
     [x: string]: unknown
 }
 
-export function Input({textarea, id, label, error, ...input}: Props) {
-    const element = textarea
-        ? <textarea id={id} {...input}/>
-        : <input id={id} type="text" {...input}/>
-
+export const Input = forwardRef<HTMLInputElement, Props>(
+    ({id, label, error, ...input}, ref) => {
     return (
         <div className={`${styles.container} ${error ? styles.danger : ""}`}>
             <label htmlFor={id}>{label}</label>
-            {element}
+            <input id={id} type="text" {...input} ref={ref}/>
             {Boolean(error) && <span>{error}</span>}
         </div>
     )
-}
+})
