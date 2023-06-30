2a4d84c426db112e32311ff571944ca8d106de11
Mateusz Marcinkowski <98377071+matevsh@users.noreply.github.com>
2023-06-30 19:44:34 +0200

    feat: textarea reuseable component

---
+++ packages/frontend/src/common/components/text-area/TextArea.tsx
@@ -0,0 +1,20 @@
+import styles from './text-area.module.css'
+import {forwardRef} from "react";
+
+interface Props {
+    id: string
+    label?: string
+    error: string | undefined
+    [x: string]: unknown
+}
+
+export const TextArea = forwardRef<HTMLTextAreaElement, Props>(
+    ({id, label, error, ...input}, ref) => {
+    return (
+        <div className={`${styles.container} ${error ? styles.danger : ""}`}>
+            <label htmlFor={id}>{label}</label>
+            <textarea id={id} {...input} ref={ref}/>
+            {Boolean(error) && <span>{error}</span>}
+        </div>
+    )
+})
